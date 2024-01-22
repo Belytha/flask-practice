b308842df70a8e581c51a41961d1551c59726bb4
Belytha <belythasnyder@gmail.com>
2024-01-22 18:52:23 -0500

    made greet and calc servers

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,31 @@
-# Put your app in here.
+from flask import Flask, request
+app = Flask(__name__)
+
+"""Basic math operations."""
+@app.route("/add")
+def add():
+    """Add a and b."""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a + b)
+
+@app.route("/sub")
+def sub():
+    """Subtract a and b."""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a - b)
+
+@app.route("/mult")
+def mult():
+    """Multiply a and b."""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a * b)
+
+@app.route("/div")
+def div():
+    """Divide a and b."""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a / b)
